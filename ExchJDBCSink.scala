2a40ae5102ed76b6619ecae9abcee3e1bcdb6197
Ryan Shin <1982sdr@hanmail.net>
2018-08-28 22:21:36 +0000

    cryptogazua crawl - ExchJDBCSink.scala 시세 데이터에 대해 insert 실패시 update 하도록 쿼리 수정.

--- ExchJDBCSink.scala
+++ ExchJDBCSink.scala
@@ -5,18 +5,16 @@ class ExchJDBCSink(url:String, user:String, pwd:String) extends ForeachWriter[Ro
     val driver = "org.mariadb.jdbc.Driver"
     var connection:Connection = _
     var statement:Statement = _
-    val sql = "INSERT INTO quote VALUES(?,?,?,?,?,?)"
+    val sql = "INSERT INTO quote VALUES(?,?,?,?,?,?) ON DUPLICATE KEY UPDATE open=?,high=?,low=?,close=?"
 
     def open(partitionId: Long, version: Long): Boolean = {
         Class.forName(driver)
         connection = DriverManager.getConnection(url, user, pwd)
         statement = connection.createStatement
-        //println(sql)
         true
     }
 
     def process(value: Row): Unit = {
-        //println(value)
         val pstmt = connection.prepareStatement(sql)
         pstmt.setString(1, value(0).toString)
         pstmt.setString(2, value(1).toString)
@@ -24,6 +22,10 @@ class ExchJDBCSink(url:String, user:String, pwd:String) extends ForeachWriter[Ro
         pstmt.setDouble(4, value(3).toString.toDouble)
         pstmt.setDouble(5, value(4).toString.toDouble)
         pstmt.setDouble(6, value(5).toString.toDouble)
+        pstmt.setDouble(7, value(2).toString.toDouble)
+        pstmt.setDouble(8, value(3).toString.toDouble)
+        pstmt.setDouble(9, value(4).toString.toDouble)
+        pstmt.setDouble(10, value(5).toString.toDouble)
         pstmt.executeUpdate()
     }
 
