c6c273c3002f51544e4765790777ae7480c2b367
Ryan Shin <1982sdr@hanmail.net>
2019-09-16 12:49:19 +0000

    cryptogazua crawl - modify some files for spark ver 2.4.4 diff --git a/build.sbt b/build.sbt index c8d44f2..4543784 100644 --- a/build.sbt +++ b/build.sbt @@ -4,4 +4,4 @@ version := "1.0"
     scalaVersion := "2.11.8"
    
    -libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.1" +libraryDependencies +=
    "org.apache.spark" %% "spark-sql" % "2.4.4" diff --git a/exchsub.sh b/exchsub.sh index 5534e52..2dde1df
    100755 --- a/exchsub.sh +++ b/exchsub.sh @@ -1 +1 @@ -$1 $SPARK_HOME/bin/spark-submit --master local[*]
    --class exchsub --driver-class-path mariadb-java-client-2.2.6.jar --jars mariadb-java-client-2.2.6.jar
    --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 target/scala-2.11/simple-project_2.11-1.0.jar
    2>/dev/null +$1 $SPARK_HOME/bin/spark-submit --master local[*] --class exchsub --driver-class-path
    mariadb-java-client-2.2.6.jar --jars mariadb-java-client-2.2.6.jar --packages
    org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4 target/scala-2.11/simple-project_2.11-1.0.jar 2>/dev/null
    diff --git a/sub.sh b/sub.sh index 468b86c..767f577 100755 --- a/sub.sh +++ b/sub.sh @@ -1 +1 @@ -$1
    $SPARK_HOME/bin/spark-submit --master local[*] --class sub --driver-class-path
    mariadb-java-client-2.2.6.jar --jars mariadb-java-client-2.2.6.jar --packages
    org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 target/scala-2.11/simple-project_2.11-1.0.jar 2>/dev/null
    +$1 $SPARK_HOME/bin/spark-submit --master local[*] --class sub --driver-class-path
    mariadb-java-client-2.2.6.jar --jars mariadb-java-client-2.2.6.jar --packages
    org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4 target/scala-2.11/simple-project_2.11-1.0.jar 2>/dev/null

--- build.sbt
+++ build.sbt
@@ -4,4 +4,4 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.1"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
--- sub.sh
+++ sub.sh
@@ -1,1 +1,1 @@
-$1 $SPARK_HOME/bin/spark-submit --master local[*] --class sub --driver-class-path mariadb-java-client-2.2.6.jar --jars mariadb-java-client-2.2.6.jar --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 target/scala-2.11/simple-project_2.11-1.0.jar 2>/dev/null
+$1 $SPARK_HOME/bin/spark-submit --master local[*] --class sub --driver-class-path mariadb-java-client-2.2.6.jar --jars mariadb-java-client-2.2.6.jar --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4 target/scala-2.11/simple-project_2.11-1.0.jar 2>/dev/null
